f319bcb7f944f1c8ad65d20fa795620c26990df1
Scott Smith <scottnelsonsmith@gmail.com>
2015-05-24 13:57:01 -0700

    Restore to original

--- mix.exs
+++ mix.exs
@@ -29,11 +29,11 @@ defmodule Ipn.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:gproc, "~> 0.3.1"},
-      {:cowboy, "~> 1.0.0"},
-      {:plug, "~> 0.10.0"},
-      {:meck, "~> 0.8.2"},
-      {:httpoison, "~> 0.4.3"}
+      {:gproc, "0.3.1"},
+      {:cowboy, "1.0.0"},
+      {:plug, "0.10.0"},
+      {:meck, "0.8.2", only: :test},
+      {:httpoison, "0.4.3", only: :test}
     ]
   end
 end
